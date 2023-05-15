33d4c781545a6a53fa02360d08184373d9f54cf9
Ilya Golod <Ilya_Golod@mckcapital.ru>
2023-05-16 00:19:30 +0300

    first commit

---
+++ slider.js
@@ -0,0 +1,17 @@
+// document.querySelectorAll('.range_box input').forEach(item => {
+
+//   item.addEventListener('mousemove', function() {
+
+//       this.closest('.range_box').querySelector('.rangeValue').innerHTML = this.value;
+//   })
+
+// });
+
+// document.querySelector('.range_box > span').innerHTML = document.querySelectorAll('.range_box input').forEach(item => {
+
+//   item.addEventListener('mousemove', function() {
+
+//       this.closest('.range_box').querySelector('.rangeValue').innerHTML = this.value;
+//   })
+
+// });
